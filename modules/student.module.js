f630005e6ae5783d99ec702a498a0e3ffe3a257a
Kuol Magok <heiskuol001@gmail.com>
2026-02-17 19:13:46 +0300

    add: student schema is created and exported

---
+++ modules/student.module.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const studentSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        RegNo: {
+            type: String,
+            required: true
+        },
+        tuitionCleared: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+ export default studentSchema
