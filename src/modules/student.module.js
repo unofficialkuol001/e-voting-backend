4e4a35de53105b49e5fc543a2b56ce6889de422f
Kuol Magok <heiskuol001@gmail.com>
2026-03-09 21:56:53 +0300

    add: create student module with mongoose schema

---
+++ src/modules/student.module.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const studentSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        regNo: {
+            type: String,
+            required: true
+        },
+        tuitionCleared: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Student = mongoose.model('Student', studentSchema);
+
+export default Student;
