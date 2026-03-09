cae64242088c47cf51e6fea8288055fb6f644883
Kuol Magok <heiskuol001@gmail.com>
2026-03-09 21:55:58 +0300

    add: implement candidate module with mongoose schema

---
+++ src/modules/candidate.module.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import Election from './election.module.js';
+
+
+const candidateSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        electionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Election"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Candidate = mongoose.model('Candidate', candidateSchema);
+
+export default Candidate
