d9b0c1c5caf1d659b353282d5c9364a2470bfbde
Kuol Magok <heiskuol001@gmail.com>
2026-03-09 21:57:11 +0300

    add: create election module with mongoose schema

---
+++ src/modules/election.module.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const electionSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        startDate: {
+            type: Date(),
+            required: true
+        },
+        endDate: {
+            type: Date(),
+            required: true
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Election = mongoose.model('Election', electionSchema);
+
+export default Election;
