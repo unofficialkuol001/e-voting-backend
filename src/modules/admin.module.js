8e8367eab7fa44164261774b94faf3d663a9a6e5
Kuol Magok <heiskuol001@gmail.com>
2026-03-11 01:05:25 +0300

    add: create admin module with mongoose schemas for Admin and AdminLogin

---
+++ src/modules/admin.module.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+
+const adminSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+
+const adminLoginSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+const AdminLogin = mongoose.model("AdminLogin", adminLoginSchema);
+
+export default { AdminLogin, Admin}
