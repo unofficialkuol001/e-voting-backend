dd6184343f15316fe67b5b3ebcf7723f8a45bf80
Kuol Magok <heiskuol001@gmail.com>
2026-03-14 01:11:33 +0300

    refactor: clean up admin and adminLogin schema definitions and export syntax

--- src/modules/admin.module.js
+++ src/modules/admin.module.js
@@ -1,41 +1,41 @@
 import mongoose from 'mongoose'
 
 const adminSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
+{
+    name: {
+        type: String,
+        required: true
     },
-    {
-        timestamps: true
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
+},
+{
+    timestamps: true
+}
 )
 
-const Admin = mongoose.model("Admin", adminSchema);
+const Admin = mongoose.model("Admin", adminSchema)
 
 
 const adminLoginSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        }
+{
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
+}
 )
 
-const AdminLogin = mongoose.model("AdminLogin", adminLoginSchema);
+const AdminLogin = mongoose.model("AdminLogin", adminLoginSchema)
 
-export default { AdminLogin, Admin}
+export { Admin, AdminLogin }
