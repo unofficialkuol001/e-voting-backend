b511f49402432532c130e8d4ea8c4e7117b66d77
Kuol Magok <heiskuol001@gmail.com>
2026-03-14 22:44:52 +0300

    feat: add route to retrieve votes by election

--- src/routes/vote.route.js
+++ src/routes/vote.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import voteController from '../controllers/vote.controller.js'
+import {voteController, getVotesByElection } from '../controllers/vote.controller.js'
 
 const router = express.Router();
 
 router.post('/', voteController);
+router.get('/election/:electionId', getVotesByElection);
 
 export default router;
